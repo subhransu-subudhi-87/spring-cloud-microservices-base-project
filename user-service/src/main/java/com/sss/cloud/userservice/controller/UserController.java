7481392934e00be43b206f76dc8008b24e9a8820
subhransu-subudhi-87 <subhransu-subudhi87@gmail.com>
2022-02-21 21:12:13 +0530

    user-microservice base project pushed

---
+++ user-service/src/main/java/com/sss/cloud/userservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.sss.cloud.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-service")
+public class UserController {
+	
+	@GetMapping("/status-check")
+	public String status() {
+	  return "Working!!!";	
+	}
+
+}
